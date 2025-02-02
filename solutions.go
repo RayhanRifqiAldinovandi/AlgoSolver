3ab0eabbdba1800572b0b58435d2a7df911b6666
Rayhan Rifqi <rayhan.aldinovandi@student.president.ac.id>
2025-02-02 07:41:09 +0700

    Added Reverse Integer

--- solutions.go
+++ solutions.go
@@ -1,33 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func main(){
-	
-	var array = []int{3,2,4}
-	fmt.Print(twoSum(array,6))
+func main() {
+
+	var array = []int{3, 2, 4}
+	fmt.Println(twoSum(array, 6))
+	fmt.Println(reverse_integer(23))
 }
 
-func twoSum(array1 []int, target int ) []int{
-	// var hashmap = make(map[int]int)
-	// for i := 0; i < len(array1);i++{
-	// 	var difference int = target - array1[i];
-	// 	// value, ok:= hashmap[difference]
-	// 	if _, exists:=hashmap[difference];exists{
-	// 		nums := []int{hashmap[difference],i}
-	// 		return nums
-	// 	}
-	// 	hashmap[array1[i]] = i
-
-	// }
-
-	// return []int{}
+func twoSum(array1 []int, target int) []int {
 
 	var hashmap = make(map[int]int)
-	for i := 0; i < len(array1); i++{
+	for i := 0; i < len(array1); i++ {
 		difference := target - array1[i]
-		if _, exists := hashmap[difference];exists{
-			return []int{hashmap[difference],i}
+		if _, exists := hashmap[difference]; exists {
+			return []int{hashmap[difference], i}
 		}
 		hashmap[array1[i]] = i
 	}
@@ -35,6 +26,27 @@ func twoSum(array1 []int, target int ) []int{
 	return []int{}
 }
 
-func duplicate([]) int{
+func reverse_integer(number int) bool {
+	var reverse_int int
+	//store the original number to be compared
+	var original_number int = number
+	for number != 0 {
+		//modulo of the number is
+		var digit int = number % 10
 
+		//Divide number with 10
+		number = number / 10
+
+		//If overflow happened
+		if reverse_int > math.MaxInt32 || reverse_int < math.MinInt32 {
+			fmt.Print("Overflow")
+			return false
+		}
+		reverse_int = reverse_int*10 + digit
+	}
+	if reverse_int == original_number {
+		return true
+	} else {
+		return false
+	}
 }
